800e83e5e8df6307e1a6a5635606f7ed98d9f8f6
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-05-08 00:00:00 +0200

    Added a navigation link to browse public tags.

---
+++ src/Site/Navigation/Link/BrowseTags.php
@@ -0,0 +1,48 @@
+<?php
+namespace Folksonomy\Site\Navigation\Link;
+
+use Omeka\Api\Representation\SiteRepresentation;
+use Omeka\Site\Navigation\Link\LinkInterface;
+use Omeka\Stdlib\ErrorStore;
+
+class BrowseTags implements LinkInterface
+{
+    public function getName()
+    {
+        return 'Browse Tags'; // @translate
+    }
+
+    public function getFormTemplate()
+    {
+        return 'common/navigation-link-form/browse-tags';
+    }
+
+    public function isValid(array $data, ErrorStore $errorStore)
+    {
+        return true;
+    }
+
+    public function getLabel(array $data, SiteRepresentation $site)
+    {
+        return isset($data['label']) && '' !== trim($data['label'])
+            ? $data['label']
+            : $this->getName();
+    }
+
+    public function toZend(array $data, SiteRepresentation $site)
+    {
+        return [
+            'route' => 'site/tags',
+            'params' => [
+                'site-slug' => $site->slug(),
+            ],
+        ];
+    }
+
+    public function toJstree(array $data, SiteRepresentation $site)
+    {
+        return [
+            'label' => $data['label'],
+        ];
+    }
+}
