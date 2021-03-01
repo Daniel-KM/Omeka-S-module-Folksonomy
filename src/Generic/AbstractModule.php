7e2a58c8b15e13c55f82e8658c9309438cb8db0e
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-03-01 00:00:00 +0000

    Updated Generic module.

--- src/Generic/AbstractModule.php
+++ src/Generic/AbstractModule.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * Copyright Daniel Berthereau, 2018-2020
+ * Copyright Daniel Berthereau, 2018-2021
  *
  * This software is governed by the CeCILL license under French law and abiding
  * by the rules of distribution of free software.  You can use, modify and/ or
@@ -181,7 +181,7 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
             return '';
         }
 
-        $form = $services->get('FormElementManager')->get($formClass);
+        $form = $formManager->get($formClass);
         $form->init();
         $form->setData($data);
         $form->prepare();
@@ -249,9 +249,6 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
         }
     }
 
-    /**
-     * @return string
-     */
     protected function modulePath(): string
     {
         return OMEKA_PATH . '/modules/' . static::NAMESPACE;
